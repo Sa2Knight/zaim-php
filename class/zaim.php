98ab5204fbe2c296e8f0156989cdffeaa49a422d
笹木信吾 <shingo.sasaki.0529@gmail.com>
2016-09-27 05:43:14 +0900

    OAuth認証関連の定数を定義

--- class/zaim.php
+++ class/zaim.php
@@ -1,8 +1,18 @@
 <?php
 
+const SITE_URL = "https://api.zaim.net";
+const API_URL = "https://api.zaim.net/v2/";
+const REQUEST_TOKEN_PATH = "/v2/auth/request";
+const AUTHORIZE_URL = "https://auth.zaim.net/users/auth";
+const ACCESS_TOKEN_PATH = "https://api.zaim.net";
+
 class Zaim
 {
 
+	// インスタンス生成時に、Oauth認証を行う
+	function __construct() {
+
+	}
 }
 
 ?>
