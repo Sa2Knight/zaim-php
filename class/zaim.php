d4df995897ca9d0b82565643b150bb81ceaad596
笹木信吾 <shingo.sasaki.0529@gmail.com>
2016-09-27 12:16:22 +0900

    やっぱりクラスに戻した

---
+++ class/zaim.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once('HTTP/OAuth/Consumer.php');
+require 'util.php';
+
+const SITE_URL = "https://api.zaim.net";
+const API_URL = "https://api.zaim.net/v2/";
+const REQUEST_TOKEN_PATH = "/v2/auth/request";
+const AUTHORIZE_URL = "https://auth.zaim.net/users/auth";
+const ACCESS_TOKEN_PATH = "https://api.zaim.net";
+
+class Zaim {
+
+	private $consumer;
+
+	// インスタンス生成時に、OAuth認証を行う
+	public function __construct() {
+		//下準備
+		session_start();
+		$keys = load_keys_file();
+		$this->consumer = new HTTP_OAuth_Consumer($keys["key"], $keys["secret"]);
+
+		//SSL設定
+		$http_request = new HTTP_Request2();
+		$http_request->setConfig('ssl_verify_peer', false);
+		$consumer_request = new HTTP_OAuth_Consumer_Request;
+		$consumer_request->accept($http_request);
+		$this->consumer->accept($consumer_request);
+
+		//アクセストークン読み込み
+		$this->consumer->setToken($keys['access_token']);
+		$this->consumer->setTokenSecret($keys['access_token_secret']);
+	}
+
+	// ユーザ名を取得
+	public function user_name() {
+		return $this->get_verify()['name'];
+	}
+
+	// ユーザ情報を取得
+	private function get_verify() {
+		return $this->get('home/user/verify')['me'];
+	}
+
+	// リクエストをGETで送信し、レスポンスのJSONを連想配列に変換して戻す
+	private function get($url) {
+		$response = $this->consumer->sendRequest(API_URL . $url , array() , 'GET');
+		$json = $response->getBody();
+		return json_decode($json , true);
+	}
+
+}
+
+?>
