8b1138e731717e3c75efbcd3b182360928dceace
笹木信吾 <shingo.sasaki.0529@gmail.com>
2016-09-28 19:07:59 +0900

    トップページに日別集計を追加

--- views/index.php
+++ views/index.php
@@ -30,20 +30,24 @@
 	</table>
 
 	<h1>集計情報</h1>
+	<p>日別集計</p>
+	<ul>
+		<li><a class="link" href="/daily.php">累計</a></li>
+	</ul>
 	<p>月別集計</p>
 	<ul>
-		<li><a href="/monthly.php?link=累計">累計</a></li>
-		<li><a href="/monthly.php?category_id=101&link=食費">食費</a></li>
-		<li><a href="/monthly.php?genre_id=10502&link=電気代">電気代</a></li>
-		<li><a href="/monthly.php?genre_id=10503&link=ガス代">ガス代</a></li>
-		<li><a href="/monthly.php?genre_id=10501&link=水道代">水道代</a></li>
-		<li><a href="/monthly.php?genre_id=9047786&comment=ポケモンGO&link=ポケモンGO">ポケモンGO</a></li>
-		<li><a href="/monthly.php?genre_id=10203&link=デグー関連">デグー関連</a></li>
+		<li><a class="link" href="/monthly.php?link=累計">累計</a></li>
+		<li><a class="link" href="/monthly.php?category_id=101&link=食費">食費</a></li>
+		<li><a class="link" href="/monthly.php?genre_id=10502&link=電気代">電気代</a></li>
+		<li><a class="link" href="/monthly.php?genre_id=10503&link=ガス代">ガス代</a></li>
+		<li><a class="link" href="/monthly.php?genre_id=10501&link=水道代">水道代</a></li>
+		<li><a class="link" href="/monthly.php?genre_id=9047786&comment=ポケモンGO&link=ポケモンGO">ポケモンGO</a></li>
+		<li><a class="link" href="/monthly.php?genre_id=10203&link=デグー関連">デグー関連</a></li>
 	</ul>
 	<p>ランキング</p>
 	<ul>
-		<li><a href="/ranking.php?target=category">カテゴリ</a></li>
-		<li><a href="/ranking.php?target=genre">ジャンル</a></li>
-		<li><a href="/ranking.php?target=place">支払先</a></li>
+		<li><a class="link" href="/ranking.php?target=category">カテゴリ</a></li>
+		<li><a class="link" href="/ranking.php?target=genre">ジャンル</a></li>
+		<li><a class="link" href="/ranking.php?target=place">支払先</a></li>
 	</ul>
 <?php require_once("footer.html"); ?>
